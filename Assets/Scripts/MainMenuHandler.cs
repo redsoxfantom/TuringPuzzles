e89235f97cdf144d678542e79673a1a67ed2c80f
Tom <redsoxfantom@gmail.com>
2016-04-05 18:56:27 -0400

    initial commit

---
+++ Assets/Scripts/MainMenuHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuHandler : MonoBehaviour {
+
+	public Button NewGameButton;
+	public Canvas MainMenuCanvas;
+	public Canvas GameCanvas;
+
+	// Use this for initialization
+	void Start () {
+		MainMenuCanvas.enabled = true;
+		GameCanvas.enabled = false;
+		NewGameButton.onClick.AddListener (newGameClicked);
+	}
+
+	void newGameClicked()
+	{
+		GameCanvas.enabled = true;
+		MainMenuCanvas.enabled = false;
+	}
+}
