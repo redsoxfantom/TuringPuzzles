d8559defd2c97131d82ccedb8299ae595d47e4a5
Tom <redsoxfantom@gmail.com>
2016-04-05 21:05:01 -0400

    Work on tool buttons

--- Assets/Scripts/ToolButton.cs
+++ Assets/Scripts/ToolButton.cs
@@ -1,15 +1,28 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class ToolButton : MonoBehaviour {
 
+	private Sprite ToolTexture;
+
 	// Use this for initialization
 	void Start () {
-	
+		ToolTexture = GetComponentInParent<Image>().overrideSprite;
+		GetComponentInParent<Toggle> ().onValueChanged.AddListener (OnToggle);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnToggle(bool newVal)
+	{
+		if (newVal == true) {
+			Cursor.SetCursor (ToolTexture.texture, Vector2.zero, CursorMode.Auto);
+		} else {
+			Cursor.SetCursor(null,Vector2.zero,CursorMode.Auto);
+		}
 	}
 }
