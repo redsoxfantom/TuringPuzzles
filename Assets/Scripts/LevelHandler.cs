a47299d692eb1955490df96b9cf25b3f29894401
Tom <redsoxfantom@gmail.com>
2016-04-06 20:28:50 -0400

    Added ability to load and parse levels

---
+++ Assets/Scripts/LevelHandler.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SimpleJSON;
+
+public class LevelHandler : MonoBehaviour {
+
+	private List<Level> LoadedLevels;
+
+	// Use this for initialization
+	void Start () {
+		var levels = Resources.LoadAll ("Levels");
+		LoadedLevels = new List<Level> ();
+
+		foreach (var level in levels) {
+			var rawJson = JSON.Parse(level.ToString());
+
+			Level deserializedLevel = new Level();
+			deserializedLevel.ChallangeText = rawJson["ChallangeText"];
+			var rawTestStrings = rawJson["TestStrings"].AsArray;
+			List<LevelTestString> testStrings = new List<LevelTestString>();
+			foreach(var rawTestString in rawTestStrings.Childs)
+			{
+				string TestString = rawTestString["TestString"].Value;
+				bool ShouldPass = rawTestString["ShouldPass"].AsBool;
+				LevelTestString deserializedTestString = new LevelTestString();
+				deserializedTestString.ShouldPass = ShouldPass;
+				deserializedTestString.TestString = TestString;
+
+				testStrings.Add(deserializedTestString);
+			}
+			deserializedLevel.TestStrings = testStrings.ToArray();
+
+			LoadedLevels.Add(deserializedLevel);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+public class Level
+{
+	public string ChallangeText{get;set;}
+	public LevelTestString[] TestStrings{get;set;}
+}
+
+public class LevelTestString
+{
+	public string TestString{ get; set; }
+	public bool ShouldPass{ get; set; }
+}
