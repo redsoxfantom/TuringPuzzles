30e917353e744bbe2f9818214e148f4cfb00aa07
Tom <redsoxfantom@gmail.com>
2016-04-08 10:03:59 -0400

    Added ability to create buttons on level load

--- Assets/Scripts/GameGridHandler.cs
+++ Assets/Scripts/GameGridHandler.cs
@@ -5,12 +5,21 @@ using UnityEngine.UI;
 public class GameGridHandler : MonoBehaviour {
 
 	public Text challangeText;
+	public GameObject gamePanel;
+	public GameGridButton gameGridButton;
+
+	private GameGridButton[,] gameGridButtons;
 	private LevelHandler levelHandler;
+	private ToolHandler toolHandler;
 	private Level currLevel;
+	private RectTransform gamePanelRect;
 
 	// Use this for initialization
 	void Start () {
 		levelHandler = GetComponent<LevelHandler> ();
+		toolHandler = GetComponent<ToolHandler> ();
+		gamePanelRect = gamePanel.GetComponent<RectTransform> ();
+
 		levelHandler.TryGetNextLevel (out currLevel);
 		LoadLevel ();
 	}
@@ -23,5 +32,18 @@ public class GameGridHandler : MonoBehaviour {
 	void LoadLevel()
 	{
 		challangeText.text = currLevel.ChallangeText;
+
+		gameGridButtons = new GameGridButton[9,9];
+		for(int x = 0; x < 9; x++)
+		{
+			for(int y = 0; y < 8; y++)
+			{
+				GameGridButton btn = (GameGridButton)Instantiate(gameGridButton, gamePanelRect.position + new Vector3(x-4.5f,y-3.5f,0), Quaternion.identity);
+				btn.transform.SetParent(gamePanel.transform);
+				btn.transform.localScale = new Vector3(1f,1f,0f);
+				btn.toolHandler = toolHandler;
+				gameGridButtons[x,y] = btn;
+			}
+		}
 	}
 }
